dd641a83c36867c2491dc9d88ae9ad8f2bba6a7a
Jon Packer <contact@jonpacker.com>
2013-10-29 16:51:50 +0100

    Revert tests to documented API

--- test/model_test.js
+++ test/model_test.js
@@ -1,5 +1,5 @@
 var assert = require('assert');
-var model = require('../').model;
+var model = require('../');
 var Emitter = require('events').EventEmitter;
 var util = require('util');
 var seraph = require('disposable-seraph');
