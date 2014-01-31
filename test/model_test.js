b2295f32ed60b946f84bef7ebcc6af65456747d1
Jon Packer <contact@jonpacker.com>
2014-01-31 10:25:59 +0100

    add test for supplying options to `where`

--- test/model_test.js
+++ test/model_test.js
@@ -262,6 +262,28 @@ describe('Seraph Model', function() {
     });
   });
 
+  it('should allow "where" queries with options', function(done) {
+    var beer = model(db, 'Beer' + Date.now());
+
+    async.forEach(_.range(0,25), function(num, callback) {
+      beer.save({name:'amazing duplicate beer',sn:Math.ceil(Math.random() * 100000)}, callback);
+    }, function(err) {
+      beer.where({ name: 'amazing duplicate beer' }, {
+        varName: "beer",
+        skip: 5,
+        limit: 15,
+        orderBy: 'beer.sn DESC'
+      }, function(err, nodes) {
+        assert(!err);
+        assert(nodes.length == 15);
+        for (var i = 0; i + 1 < nodes.length; ++i) {
+          assert(nodes[i].sn > nodes[i + 1].sn);
+        }
+        done();
+      });
+    });
+  });
+
   it('should save a model with a string id', function(done) {
     var beer = model(db, 'Beer');
     var food = model(db, 'Food');
